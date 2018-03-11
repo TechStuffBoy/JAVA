3aae131b7cd5ed557d85bc92b5681752102a1337
Nandha_Kumar_M <nandhakumar@cpsgateway.com>
2018-03-11 15:47:07 +0530

    Static Tutorial added

---
+++ TestProjects/src/com/StaticKeyword1.java
@@ -0,0 +1,23 @@
+package com;
+
+class Counter {
+	int count1 = 0; 
+	static int staticCount; //Static 
+	
+	public Counter() {
+		count1++;
+		staticCount++;
+		System.out.println("Count1=" + count1 +  " StaticCount=" + staticCount);
+	}
+}
+
+public class StaticKeyword1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Counter c1 = new Counter();
+		Counter c2 = new Counter();
+		Counter c3 = new Counter();
+	}
+
+}
---
+++ TestProjects/src/com/StaticMethod.java
@@ -0,0 +1,41 @@
+package com;
+
+class StaticStudent {
+	int rollNo;
+	String name;
+	static String clgName = "IIPM";
+	
+	public StaticStudent(int iRollNo, String iName) {
+		rollNo = iRollNo;
+		name = iName;
+	}
+	static void changeCollege(String iCollege){
+		clgName = iCollege;
+	}
+	void displyInfo(){
+		System.out.println("RollNo= " + rollNo + " name=" 
+				+ name + " clgName=" + clgName);
+	}
+	//Static method to calculate square of no
+	static int calculateSquare(int no) {
+		return no*no;
+	}
+}
+public class StaticMethod {
+
+	public static void main(String[] args) {
+		StaticStudent nandhu = new StaticStudent(123,"Nandhu");
+		nandhu.displyInfo();
+		StaticStudent.changeCollege("IIT");
+		nandhu.displyInfo();
+		System.out.println("Square of 48 = " + StaticStudent.calculateSquare(48));
+		
+	}
+
+}
+
+/*
+There are two main restrictions for the static method. They are:
+1) The static method can not use non static data member or call non-static method directly.
+2) this and super cannot be used in static context.
+*/
